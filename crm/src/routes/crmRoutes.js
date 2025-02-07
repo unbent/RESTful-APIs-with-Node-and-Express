ef839dde43aa2ba4cdfb72e284be8fc7d9649fc5
unbent <daniel.polan@hotmail.com>
2025-02-07 09:42:21 -0500

    Create basic middleware for routes

--- crm/src/routes/crmRoutes.js
+++ crm/src/routes/crmRoutes.js
@@ -1,23 +1,58 @@
+import { request, response } from 'express'
+
 const routes = (app) => {
   app
     .route('/contact')
-    .get((request, response) => {
-      response.send('GET request successful!')
-    })
+    .get(
+      (request, response, next) => {
+        //middleware
+        logRequestDetails(request)
+        next()
+      },
+      (request, response, next) => {
+        response.send('GET request successful!')
+      }
+    )
 
-    .post((request, response) => {
-      response.send('POST request successful!')
-    })
+    .post(
+      (request, response, next) => {
+        //middleware
+        logRequestDetails(request)
+        next()
+      },
+      (request, response, next) => {
+        response.send('POST request successful!')
+      }
+    )
 
   app
     .route('/contact/:contactId')
-    .put((request, response) => {
-      response.send('PUT request successful!')
-    })
+    .put(
+      (request, response, next) => {
+        //middleware
+        logRequestDetails(request)
+        next()
+      },
+      (request, response, next) => {
+        response.send('PUT request successful!')
+      }
+    )
 
-    .delete((request, response) => {
-      response.send('DELETE request successful!')
-    })
+    .delete(
+      (request, response, next) => {
+        //middleware
+        logRequestDetails(request)
+        next()
+      },
+      (request, response, next) => {
+        response.send('DELETE request successful!')
+      }
+    )
+}
+
+function logRequestDetails(request) {
+  console.log(`Request from ${request.originalUrl}`)
+  console.log(`Request type ${request.method}`)
 }
 
 export default routes
